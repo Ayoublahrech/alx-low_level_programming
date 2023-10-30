58c153b9f8c88d579c196ce4557fa57a7ccb010a
Ayoublahrech <lahrech.ayoub33@gmail.com>
2023-10-30 05:52:13 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,45 +1,28 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * create_array - create array of size size and assign char c
-		
- * @size: size of array
-		
- * @c: char to assign
-		
- * Description: creat array of size size and assign char c
-		
- * Return: pointer to array, NULL if fail
-		
+ * create_array - Create an array of characters and initialize it with a specific character.
+ * @size: The size of the array to create.
+ * @c: The character to initialize the array with.
  *
-		
+ * Description: This function allocates an array of characters of size 'size' and initializes
+ * each element with the character 'c'.
+ *
+ * Return: If successful, a pointer to the created array; otherwise, NULL.
  */
-		
 char *create_array(unsigned int size, char c)
-		
 {
-		
 	char *str;
-		
 	unsigned int i;
-		
 
-		
 	str = malloc(sizeof(char) * size);
-		
-	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
 
-		
+	if (size == 0 || str == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
-		
 		str[i] = c;
-		
+
 	return (str);
-		
 }
