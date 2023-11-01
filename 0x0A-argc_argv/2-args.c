6a4a2053b2157a415f2573a028bc098cc99eb833
Ayoublahrech <lahrech.ayoub33@gmail.com>
2023-11-01 07:53:42 -0700

    betty style correction

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,13 +2,10 @@
 
 /**
  * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
  *
- * Description: This program prints all the arguments it receives, one per line.
- *
- * @argc: The number of command-line arguments
- * @argv: An array of strings containing the command-line arguments
- *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
