40cf7b201d78492a50525dc4827395164667b6a0
Ayoublahrech <lahrech.ayoub33@gmail.com>
2023-11-01 07:44:01 -0700

    betty style correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
-    (void)argv;  /* Prevent unused variable warning */
+	(void)argv;  /* Prevent unused variable warning */
 
-    printf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 
-    return (0);
+	return (0);
 }
