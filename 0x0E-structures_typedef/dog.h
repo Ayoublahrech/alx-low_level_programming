b5d2962fc428b5e23c7a0cb248a8554e20324459
Ayoublahrech <lahrech.ayoub33@gmail.com>
2023-11-05 05:20:51 -0800

    betty style correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,67 +1,58 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
- */
-struct dog
-{
-    char *name;
-    float age;
-    char *owner;
-};
-
-
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
-
-
-/**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
- */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-
-/**
- * print_dog - prints information about a dog
- * @d: pointer to struct dog to print information about
- */
-void print_dog(struct dog *d);
-
-
-/**
- * new_dog - creates a new struct dog
+ * struct dog - A dog's basic information
  * @name: The dog's name
  * @age: The dog's age
  * @owner: The dog's owner
  *
- * Description: Allocates memory for a new struct dog and initializes it with the provided name, age, and owner.
+ * Description: A struct for storing a dog's basic information.
+ */
+typedef struct dog {
+  char *name;
+  float age;
+  char *owner;
+} dog_t;
+
+/**
+ * init_dog - Initializes a dog struct
+ * @d: Pointer to the dog struct to initialize
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
  *
- * Return: Pointer to the newly created struct dog
+ * Description: Initializes the dog struct with the provided name, age, and owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - Prints information about a dog
+ * @d: Pointer to the dog struct to print information about
+ *
+ * Description: Prints the name, age, and owner of the dog to the console.
+ */
+void print_dog(struct dog *d);
+
+/**
+ * new_dog - Creates a new dog struct
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: Allocates memory for a new dog struct and initializes it with the provided name, age, and owner.
+ *
+ * Return: Pointer to the newly created dog struct
  */
 dog_t *new_dog(char *name, float age, char *owner);
 
-
 /**
- * free_dog - frees the memory associated with a struct dog
- * @d: pointer to the struct dog to free
+ * free_dog - Frees the memory associated with a dog struct
+ * @d: Pointer to the dog struct to free
  *
- * Description: Frees the memory associated with the provided struct dog.
+ * Description: Frees the memory associated with the provided dog struct.
  */
 void free_dog(dog_t *d);
-
 
 /**
  * _strcpy - Copies a string from one memory location to another
@@ -74,7 +65,6 @@ void free_dog(dog_t *d);
  */
 char *_strcpy(char *dest, char *src);
 
-
 /**
  * _strlen - Calculates the length of a string
  * @s: Pointer to the string to calculate the length of
@@ -84,6 +74,5 @@ char *_strcpy(char *dest, char *src);
  * Return: The length of the string
  */
 int _strlen(char *s);
-
 
 #endif
